ed5fd12284c938c844d4fee58792d85795040cf8
NikolaPizurica <nikola.pizurica.1998@gmail.com>
2019-02-04 01:32:35 +0100

    Added icons and the ability to edit existing objects to ObjectBuilder

--- 2D_Game_Engine/src/ObjectBuilder/BuilderWindow.java
+++ 2D_Game_Engine/src/ObjectBuilder/BuilderWindow.java
@@ -5,6 +5,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.GridLayout;
 import java.awt.Polygon;
+import java.awt.Shape;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -21,11 +22,14 @@ import java.io.IOException;
 import java.util.LinkedList;
 
 import javax.swing.ButtonGroup;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JSlider;
+import javax.swing.JTextField;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
@@ -36,8 +40,13 @@ public class BuilderWindow extends JFrame {
 	 */
 	private static final long serialVersionUID = -7879429050579315307L;
 	
+	private ImageIcon circle0, circle1, ellipse0, ellipse1, square0, square1, rectangle0, rectangle1, polygon0, polygon1;
+	
 	private JRadioButton circle, ellipse, square, rectangle, polygon;
 	private ButtonGroup group;
+	private JLabel l1;
+	private JTextField name;
+	private JButton open;
 	
 	private ColorTest ct;
 	private JPanel sliders;
@@ -62,6 +71,39 @@ public class BuilderWindow extends JFrame {
 	private int shapeCount;
 	private LinkedList<String> shapeList;
 
+	public void switchIcons() {
+		if(circle.isSelected()) {
+			circle.setIcon(circle1);
+		}
+		else {
+			circle.setIcon(circle0);
+		}
+		if(ellipse.isSelected()) {
+			ellipse.setIcon(ellipse1);
+		}
+		else {
+			ellipse.setIcon(ellipse0);
+		}
+		if(square.isSelected()) {
+			square.setIcon(square1);
+		}
+		else {
+			square.setIcon(square0);
+		}
+		if(rectangle.isSelected()) {
+			rectangle.setIcon(rectangle1);
+		}
+		else {
+			rectangle.setIcon(rectangle0);
+		}
+		if(polygon.isSelected()) {
+			polygon.setIcon(polygon1);
+		}
+		else {
+			polygon.setIcon(polygon0);
+		}
+	}
+	
 	public void setRed(int red) {
 		this.red = red;
 	}
@@ -155,37 +197,63 @@ public class BuilderWindow extends JFrame {
 		this.setResizable(false);
 		this.setLocationRelativeTo(null);
 		
-		inputType = new JPanel();
-		inputType.setLayout(new GridLayout(1, 5));
-		inputType.setPreferredSize(new Dimension(width, 40));
-		inputType.setMinimumSize(new Dimension(width, 40));
-		inputType.setMaximumSize(new Dimension(width, 40));
-		
 		rbl = new RadioButtonListener();
+		ml = new MouseListener();
+		sl = new SliderListener();
+		bl = new ButtonListener();
 		
-		circle = new JRadioButton("Circle");
+		inputType = new JPanel();
+		inputType.setLayout(new GridLayout(1, 8));
+		inputType.setPreferredSize(new Dimension(width, 50));
+		inputType.setMinimumSize(new Dimension(width, 50));
+		inputType.setMaximumSize(new Dimension(width, 50));
+		
+		l1 = new JLabel("Object name: ");
+		inputType.add(l1);
+		
+		name = new JTextField(20);
+		name.setText("object");
+		inputType.add(name);
+		
+		open = new JButton("Open");
+		open.setActionCommand("open");
+		open.addActionListener(bl);
+		inputType.add(open);
+		
+		circle0 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/circle_unselected.png");
+		circle1 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/circle_selected.png");
+		ellipse0 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/ellipse_unselected.png");
+		ellipse1 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/ellipse_selected.png");
+		square0 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/square_unselected.png");
+		square1 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/square_selected.png");
+		rectangle0 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/rectangle_unselected.png");
+		rectangle1 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/rectangle_selected.png");
+		polygon0 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/polygon_unselected.png");
+		polygon1 = new ImageIcon("./2D_Game_Engine/Assets/ButtonIcons/polygon_selected.png");
+		
+		circle = new JRadioButton(circle1);
 		circle.setActionCommand("circle");
 		circle.addActionListener(rbl);
 		circle.setSelected(true);
 		selectedType = "circle";
 		inputType.add(circle);
 		
-		ellipse = new JRadioButton("Ellipse");
+		ellipse = new JRadioButton(ellipse0);
 		ellipse.setActionCommand("ellipse");
 		ellipse.addActionListener(rbl);
 		inputType.add(ellipse);
 		
-		square = new JRadioButton("Square");
+		square = new JRadioButton(square0);
 		square.setActionCommand("square");
 		square.addActionListener(rbl);
 		inputType.add(square);
 		
-		rectangle = new JRadioButton("Rectangle");
+		rectangle = new JRadioButton(rectangle0);
 		rectangle.setActionCommand("rectangle");
 		rectangle.addActionListener(rbl);
 		inputType.add(rectangle);
 		
-		polygon = new JRadioButton("Polygon");
+		polygon = new JRadioButton(polygon0);
 		polygon.setActionCommand("polygon");
 		polygon.addActionListener(rbl);
 		inputType.add(polygon);
@@ -199,12 +267,10 @@ public class BuilderWindow extends JFrame {
 		
 		this.add(inputType, BorderLayout.NORTH);
 		
-		ml = new MouseListener();
-		
 		dp = new DrawPanel();
-		dp.setPreferredSize(new Dimension(width, height-80));
-		dp.setMinimumSize(new Dimension(width, height-80));
-		dp.setMaximumSize(new Dimension(width, height-80));
+		dp.setPreferredSize(new Dimension(width, height-110));
+		dp.setMinimumSize(new Dimension(width, height-110));
+		dp.setMaximumSize(new Dimension(width, height-110));
 		dp.addMouseListener(ml);
 		dp.addMouseMotionListener(ml);
 		dp.addMouseWheelListener(ml);
@@ -212,7 +278,7 @@ public class BuilderWindow extends JFrame {
 		
 		// questionable
 		this.x0 = width/2 - 7;
-		this.y0 = height/2 - 40 - 29;
+		this.y0 = height/2 - 55 - 20;
 		
 		controls = new JPanel();
 		controls.setLayout(new GridLayout(1, 5));
@@ -223,8 +289,6 @@ public class BuilderWindow extends JFrame {
 		ct = new ColorTest(Color.BLACK);
 		red = green = blue = 0;
 		controls.add(ct);
-		
-		sl = new SliderListener();
 		
 		sliders = new JPanel();
 		sliders.setLayout(new GridLayout(3, 1));
@@ -239,8 +303,6 @@ public class BuilderWindow extends JFrame {
 		sliders.add(b);
 		
 		controls.add(sliders);
-		
-		bl = new ButtonListener();
 		
 		undo = new JButton("Undo");
 		undo.setActionCommand("undo");
@@ -270,13 +332,72 @@ public class BuilderWindow extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			setSelectedType(e.getActionCommand());
+			switchIcons();
 		}
 	}
 	
 	private class ButtonListener implements ActionListener {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-			if(e.getActionCommand().equals("undo") && getShapeCount() > 0) {
+			if(e.getActionCommand().equals("open")) {
+				try {
+					BufferedReader bw = new BufferedReader(new FileReader(new File(path + name.getText() + ".dat")));
+					int cnt = Integer.parseInt(bw.readLine());
+					clearShapeList();
+					dp.clear();
+					setShapeCount(cnt);
+					for(int i = 0; i < cnt; i++) {
+						String line = bw.readLine();
+						bw.readLine();
+						pushShape(line);
+						String [] temp = line.split(" ");
+						if(temp[0].equals("Polygon")) {
+							String [] coords = temp[1].split(";");
+							int [] xp = new int[coords.length];
+							int [] yp = new int[coords.length];
+							for(int j = 0; j < coords.length; j++) {
+								String [] xy = coords[j].split(",");
+								xp[j] = Integer.parseInt(xy[0]) + x0;
+								yp[j] = Integer.parseInt(xy[1]) + y0;
+							}
+							Shape shape = new Polygon(xp, yp, coords.length);
+							String [] rgb = temp[2].split(",");
+							int r = Integer.parseInt(rgb[0]);
+							int g = Integer.parseInt(rgb[1]);
+							int b = Integer.parseInt(rgb[2]);
+							Color color = new Color(r, g, b);
+							dp.add(shape, color);
+						}
+						else {
+							String [] coords = temp[1].split(",");
+							int x = Integer.parseInt(coords[0]) + x0;
+							int y = Integer.parseInt(coords[1]) + y0;
+							int w = Integer.parseInt(coords[2]);
+							int h = Integer.parseInt(coords[3]);
+							Shape shape;
+							if(temp[0].equals("Circle") || temp[0].equals("Ellipse")) {
+								shape = new Ellipse2D.Float(x, y, w, h);
+							}
+							else {
+								shape = new Rectangle2D.Float(x, y, w, h);
+							}
+							String [] rgb = temp[2].split(",");
+							int r = Integer.parseInt(rgb[0]);
+							int g = Integer.parseInt(rgb[1]);
+							int b = Integer.parseInt(rgb[2]);
+							Color color = new Color(r, g, b);
+							dp.add(shape, color);
+						}
+					}
+					dp.repaint();
+					bw.close();
+				}
+				catch (IOException exc) {
+					System.out.println("Object " + name.getText() + " doesn't exist in the current workspace.");
+					//exc.printStackTrace();
+				}
+			}
+			else if(e.getActionCommand().equals("undo") && getShapeCount() > 0) {
 				popShape();
 				setShapeCount(getShapeCount() - 1);
 				try {
@@ -348,6 +469,7 @@ public class BuilderWindow extends JFrame {
 		
 		@Override
 		public void mousePressed(MouseEvent e) {
+			System.out.println((e.getX()-x0) + " " + (e.getY()-y0));
 			String type = getSelectedType();
 			if(type.equals("circle") || type.equals("ellipse") || type.equals("square") || type.equals("rectangle")) {
 				this.x1 = e.getX();
